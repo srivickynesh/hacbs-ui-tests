b31ab5bf0a2ee3d796c2c8719c898f63d51d3ad0
Sri Vignesh <vigneshsri11@gmail.com>
2022-03-11 14:39:41 +0530

    Add definite folder structure

---
+++ cypress/tests/sample/sampleTests.spec.js
@@ -0,0 +1,16 @@
+/** *****************************************************************************
+ * Licensed Materials - Property of Red Hat, Inc.
+ * Copyright (c) 2022 Red Hat, Inc.
+ ****************************************************************************** */
+
+/// <reference types="cypress" />
+
+
+describe('My Sample Test', () => {
+  it('Visits the Openshit console Page', () => {
+    cy.visit('http://console-openshift-console.apps.ci-ln-kxi1l5k-72292.origin-ci-int-gce.dev.rhcloud.com', {timeout: 60000})
+    cy.get('#inputUsername', { timeout: 20000 }).click().focused().type('kubeadmin')
+    cy.get('#inputPassword', { timeout: 20000 }).click().focused().type('your password')
+    cy.get('button[type="submit"]', { timeout: 20000 }).click()
+  })
+})
---
+++ cypress/tests/sample/testTemplate.spec.js
@@ -0,0 +1,50 @@
+/** *****************************************************************************
+ * Licensed Materials - Property of Red Hat, Inc.
+ * Copyright (c) 2021 Red Hat, Inc.
+ ****************************************************************************** */
+
+/// <reference types="cypress" />
+
+
+import { }
+
+
+const 
+
+
+describe('verify some features', {
+
+    }
+},
+function () {
+    before(function () {
+        //cy.clearCookies()
+        //cy.login()
+
+    })
+
+    it(`<testCase-id>: <component-name>:  Automation - <description>`,  function () {
+        
+        ///
+
+        //    Testcase Steps
+
+        ///
+        })
+    it(`<testCase-id>: <component-name>:  Automation - <description>`,  function () {
+        
+        ///
+
+        //    Testcase Steps
+
+        ///
+        })
+    it(`<testCase-id>: <component-name>:  Automation - <description>`,  function () {
+        
+        ///
+
+        //    Testcase Steps
+
+        ///
+        })
+})
